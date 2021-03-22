52c6033b8819ec0396876c276010a4b68512f4bb
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-03-21 20:16:10 -0500

    The function was documented

--- Init_Buffer_And_Write.c
+++ Init_Buffer_And_Write.c
@@ -1,4 +1,9 @@
-#include "Holberton.h"
+#include "holberton.h"
+
+/**
+ * Buffer_Init - The structure buffer was initializer.
+ * @buffer: Data structure that handles the 1024 buffer.
+ */
 
 void Buffer_Init(Buffer *buffer)
 {
@@ -7,9 +12,16 @@ void Buffer_Init(Buffer *buffer)
 	buffer->Length = buffer->Length_Total = 0;
 }
 
-void Write_Init
-(Write *write, void (*Function_Pointer_Write)(Buffer *, char **, int), void (*Function_Pointer_Print)(Buffer *)){
+/**
+ * Write_Init - The structure write was initializer.
+ * @write: data structure that contains two function pointers, one to write
+ *         to the buffer and the other to print the buffer.
+ * @Ptr_Write: Pointer to function.
+ * @Ptr_Print: Pointer to funciotn
+ */
 
-	write->Write = Function_Pointer_Write;
-	write->Print = Function_Pointer_Print;
+void Write_Init(Write *write, void (*Ptr_Write)(Buffer *, char **, int), void (*Ptr_Print)(Buffer *))
+{
+	write->Write = Ptr_Write;
+	write->Print = Ptr_Print;
 }
